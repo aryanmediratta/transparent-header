3c35d71b58ff0be0d54363d8c92a635923fb8026
aryanmediratta <43181596+aryanmediratta@users.noreply.github.com>
2019-02-22 00:57:59 +0530

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,28 @@
+<Fragment>
+      <br/>
+      <br/>
+      <div className = "row">
+      <div className = "col s6 m2 l2">
+      <a href="/" className="brand-logo cyan-text text-lighten-4 left" style = {ps}>Delhii Grill</a>
+      </div>
+      <div className = "col s5 m10 l10">
+      <div className="right">
+              <div className = "hide-on-small-only">
+
+           <a href="/menu" className="cyan-text text-lighten-4" style = {ps}>Menu</a>
+           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <a href="/contact" className="cyan-text text-lighten-4" style = {ps}>Contact Us</a>
+        </div>
+        <div className = "hide-on-med-and-up">
+          <a href="#" data-target="mobile-demo" className="right sidenav-trigger cyan-text text-lighten-4" ><i className="material-icons">menu</i></a>
+            <ul className="right hide-on-med-and-down" >
+              <li><a href="/menu" className="cyan-text text-lighten-4" style = {ps}>Menu</a></li>
+              <li><a href="/contact" className="cyan-text text-lighten-4" style = {ps}>Contact Us</a></li>
+            </ul>
+            </div>
+      </div>
+            <div className = "col s1 m2 l2">
+              </div>
+      </div>
+      </div>
+      </Fragment>
