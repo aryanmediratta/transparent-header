8766fb436a737b7a1ccc1059b9a4068d24f8d3ae
aryanmediratta <43181596+aryanmediratta@users.noreply.github.com>
2019-02-23 17:26:02 +0530

    Update Header.js

--- Header.js
+++ Header.js
@@ -23,32 +23,35 @@ document.addEventListener('DOMContentLoaded', function() {
 	render(){
 
 		return(	
-<Fragment>
+<div>
+      <Fragment>
       <div className = "row">
-      <div className = "col s6 m2 l2">
-      <a href="/" className="brand-logo cyan-text text-lighten-4 left" style = {ps}>Brand Name</a>
+      <div className = "col s6 m3 l3">
+      <a href="/" className="brand-logo black-text left" style = {ps}>Brand Name</a>
       </div>
-      <div className = "col s5 m10 l10">
+      <div className = "col s5 m9 l9">
       <div className="right">
               <div className = "hide-on-small-only">
 
-           <a href="/menu" className="cyan-text text-lighten-4" style = {ps}>Menu</a>
+           <a href="/menu" className="black-text" style = {ps}>Menu</a>
            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <a href="/contact" className="cyan-text text-lighten-4" style = {ps}>Contact Us</a>
+          <a href="/contact" className="black-text" style = {ps}>Contact Us</a>
         </div>
         <div className = "hide-on-med-and-up">
-          <a href="#" data-target="mobile-demo" className="right sidenav-trigger cyan-text text-lighten-4" ><i className="material-icons">menu</i></a>
-            <ul className="right hide-on-med-and-down" >
-              <li><a href="/menu" className="cyan-text text-lighten-4" style = {ps}>Menu</a></li>
-              <li><a href="/contact" className="cyan-text text-lighten-4" style = {ps}>Contact Us</a></li>
-            </ul>
+          <a href="#" data-target="mobile-demo" className="right sidenav-trigger black-text" ><i className="material-icons">menu</i></a>
+  <ul className = "sidenav" id="mobile-demo">
+        <li><a href = "/">Home</a></li>
+        <li><a href = "/menu">Menu</a></li>
+        <li><a href = "/contact">Contact Us</a></li>
+  </ul>
             </div>
       </div>
-            <div className = "col s1 m2 l2">
+            <div className = "col s1">
               </div>
       </div>
       </div>
       </Fragment>
+      </div>
                    )
 	}
 }
